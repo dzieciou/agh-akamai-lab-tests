802fbd23396a0591b8637fd465b28702ab1ed32d
mgawinec <mgawinec@akamai.com>
2016-04-11 17:17:14 +0200

    Updated tests

--- src/test/java/com/akamai/testing/aghlab/rest/PositiveBankBackendTest.java
+++ src/test/java/com/akamai/testing/aghlab/rest/PositiveBankBackendTest.java
@@ -3,21 +3,32 @@ package com.akamai.testing.aghlab.rest;
 
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+
 import org.junit.Test;
 
 
 public class PositiveBankBackendTest {
 
+    public static final String CARD_NUMBER = "4012888888881881";
+    public static final String SHOP_ACCOUNT_NUMBER = "1234";
+
     @Test
     public void forValidCardInfoTransactionShouldBeAccepted() {
+
+
+        int initialCardBalance = getCardBalance(CARD_NUMBER);
+        int initialshopAccountBalance = getAccountBalance(SHOP_ACCOUNT_NUMBER);
 
         //@formatter:off
         given().
                 log().all().
                 baseUri("http://localhost:8090").
-                formParam("target_account_iban", "9484984948948498").
+                formParam("target_account_iban", "1234").
                 formParam("amount", "600").
-                formParam("cc_number", "4012888888881881").
+                formParam("cc_number", CARD_NUMBER).
                 formParam("cc_owner", "MACIEK").
                 formParam("cc_csc", 757).
         when().
@@ -27,6 +38,41 @@ public class PositiveBankBackendTest {
                 statusCode(200);
         //@formatter:on
 
+        int cardBalance = getCardBalance(CARD_NUMBER);
+        int shopAccountBalance = getAccountBalance(SHOP_ACCOUNT_NUMBER);
+
+        assertThat(cardBalance, lessThan(initialCardBalance));
+        assertThat(shopAccountBalance, greaterThan(initialshopAccountBalance));
+    }
+
+    private int getCardBalance(String cardNumber) {
+        return
+        //@formatter:off
+        given().
+                log().all().
+                baseUri("http://localhost:8090").
+                pathParam("cardNumber", cardNumber).
+        when().
+                get("/bank/cards/{cardNumber}/balance").
+        then().
+                log().all().
+                statusCode(200).extract().jsonPath().getInt("balance");
+        //@formatter:on
+    }
+
+    private int getAccountBalance(String accountNumber) {
+        return
+                //@formatter:off
+        given().
+                log().all().
+                baseUri("http://localhost:8090").
+                pathParam("accountNumber", accountNumber).
+        when().
+                get("/bank/accounts/{accountNumber}/balance").
+        then().
+                log().all().
+                statusCode(200).extract().jsonPath().getInt("balance");
+        //@formatter:on
     }
 
 
